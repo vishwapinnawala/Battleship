902335963f43442d3837c39066b8675bad113d94
vishwapinnawala <vishwaspinnawala@gmail.com>
2023-01-13 21:20:26 +0000

    Jan 14, 2023, 2:50 AM

--- app.js
+++ app.js
@@ -110,6 +110,19 @@ document.addEventListener('DOMContentLoaded', () => {//Gettings Elements to Vari
   function dragOff(e) { e.preventDefault() }
   function dragin(e) {e.preventDefault()}
 
+  function drop() {
+    let lastselectship = selectedship.lastChild.id
+    let shipsclass = lastselectship.slice(0, -2)
+    let lastship = parseInt(lastselectship.substr(-1))
+    let lastshipid = lastship + parseInt(this.dataset.id)
+  
+    const notAllowedHorizontal = [0,10,20,30,40,50,60,70,80,90,1,11,21,31,41,51,61,71,81,91,2,22,32,42,52,62,72,82,92,3,13,23,33,43,53,63,73,83,93]
+    const notAllowedVertical = [99,98,97,96,95,94,93,92,91,90,89,88,87,86,85,84,83,82,81,80,79,78,77,76,75,74,73,72,71,70,69,68,67,66,65,64,63,62,61,60]
+    
+    let newNotAllowedHorizontal = notAllowedHorizontal.splice(0, 10 * lastship)
+    let newNotAllowedVertical = notAllowedVertical.splice(0, 10 * lastship)
 
+    
+  }
 
 })
