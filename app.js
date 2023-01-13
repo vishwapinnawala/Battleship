e760745fa756511f90c3edf7182c65741bc68955
vishwapinnawala <vishwaspinnawala@gmail.com>
2023-01-13 01:22:31 +0000

    Jan 13, 2023, 6:52 AM

--- app.js
+++ app.js
@@ -28,6 +28,17 @@ document.addEventListener('DOMContentLoaded', () => {
       blocks.push(block) 
     }
   }
+  
+
+  const shipsarray = [
+    {name: 'destroyer',directions: [[0, 1],[0, width]]},
+    {name: 'submarine',directions: [[0, 1, 2],[0, width, width*2]]},
+    {name: 'cruiser',directions: [[0, 1, 2],[0, width, width*2]]},
+    {name: 'battleship',directions: [[0, 1, 2, 3],[0, width, width*2, width*3]]},
+    {name: 'carrier',directions: [[0, 1, 2, 3, 4],[0, width, width*2, width*3, width*4]]},
+  ]
+
   generateboard(userboard, userblocks)
   generateboard(botboard, botblocks)
+
 })
