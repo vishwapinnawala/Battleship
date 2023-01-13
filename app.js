cb90e8b33c565c4c63d0697e120edd597b3f6915
vishwapinnawala <vishwaspinnawala@gmail.com>
2023-01-13 05:58:06 +0530

    dom elements

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+  const userboard = document.querySelector('.usergrid')
+  const botboard = document.querySelector('.computergrid')
+  const displayboard = document.querySelector('.shipselector')
+  const ships = document.querySelectorAll('.ships')
+  const destroyer = document.querySelector('.destroyerwrap')
+  const submarine = document.querySelector('.submarinewrap')
 })
