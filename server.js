48f058a3c62b2449e823b58074036ee0cf020051
vishwapinnawala <vishwaspinnawala@gmail.com>
2023-01-18 00:30:40 +0530

    update server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const socketio = require('socket.io')
 const app=express()
 const server=http.createServer(app)
 const io=socketio(server)
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000 
+
 
 app.use(express.static(path.join(__dirname,"Client")))
 
