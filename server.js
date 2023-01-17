71dc18db63135f197ae7a88272c32bfdca469ba5
vishwapinnawala <vishwaspinnawala@gmail.com>
2023-01-18 04:17:46 +0530

    update server.js

--- server.js
+++ server.js
@@ -6,7 +6,15 @@ const app=express()
 const server=http.createServer(app)
 const io=socketio(server)
 const port = process.env.PORT || 3000 
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
+const uri = "mongodb+srv://<username>:<password>@cluster0.3qufw4p.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 
 app.use(express.static(path.join(__dirname,"Client")))
